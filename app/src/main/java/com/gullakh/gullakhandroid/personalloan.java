c6b4188db4237c924b27d0a27092d5b74f9f612f
sandysf <sandysfcloud@gmail.com>
2016-01-25 11:35:28 +0530

    Chethan - 25-01-16 - V1.1

--- app/src/main/java/com/gullakh/gullakhandroid/personalloan.java
+++ app/src/main/java/com/gullakh/gullakhandroid/personalloan.java
@@ -1,13 +1,13 @@
-package com.gullakh.applciation;
+package com.gullakh.gullakhandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class personalloan extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_personalloan);
     }
 }
